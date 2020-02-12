e1c48ccd7a7f94735d40c0d4bd64bbaa884c2941
UltimatelamescriptkiddieDoerp <d.rossner@gmx.de>
2020-02-12 20:35:59 +0000

    testing

--- pwcracker_for_zip.py
+++ pwcracker_for_zip.py
@@ -4,7 +4,6 @@ import string
 from threading import Thread
 import traceback
 
-print("Starte Vorgang")
 
 def crack(zip, pwd):
     try:
@@ -13,7 +12,7 @@ def crack(zip, pwd):
     except Exception:
         pass
 
-zipFile = zipfile.ZipFile("/home/doerpi/Downloads/crackmeZIP.zip")
+zipFile = zipfile.ZipFile("/home/doerpi/Downloads/aaa.zip")
 myLetters = string.ascii_letters + string.digits + string.punctuation
 for i in range(3,30):
     for j in map(''.join, itertools.product('myLetters, repeat=i')):

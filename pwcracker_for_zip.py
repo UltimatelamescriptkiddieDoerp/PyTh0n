e6c1b50154397729500a9b2c6352429ec2cb5327
UltimatelamescriptkiddieDoerp <d.rossner@gmx.de>
2020-02-12 21:51:14 +0000

    finish without multithreading

--- pwcracker_for_zip.py
+++ pwcracker_for_zip.py
@@ -1,6 +1,7 @@
 import zipfile
 import itertools
 import string
+import threading
 from threading import Thread
 import traceback
 
@@ -12,9 +13,9 @@ def crack(zip, pwd):
     except Exception:
         pass
 
-zipFile = zipfile.ZipFile("/home/doerpi/Downloads/aaa.zip")
+zipFile = zipfile.ZipFile(" #Hier den Dateipfad inkl. Datei + Dateiendung einfuegen")
 myLetters = string.ascii_letters + string.digits + string.punctuation
 for i in range(3,30):
-    for j in map(''.join, itertools.product('myLetters, repeat=i')):
+    for j in map(''.join, itertools.product(myLetters, repeat=i)):
         t = Thread(target=crack, args=(zipFile, j))
         t.start()
